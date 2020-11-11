ebe1ee3bee0c1c18065a54723e652debfe1368a3
FGHTech <56099414+FGHTech@users.noreply.github.com>
2020-11-12 00:07:46 +0100

    mousescroll 0.5x

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+
+console.log("load event detected!");
+function load() {
+  console.log("load event detected!");
+  var x = document.getElementsByClassName("mouse");
+  setTimeout(function(){ 
+    x[0].style.opacity = "1";
+    x[0].style.color = "#f8f4e3";
+    x[0].style.transition = "all 1s ease 0s";
+  
+  }, 250);
+};
+window.onload = load;
